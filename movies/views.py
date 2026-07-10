03d2904e1285afe8c55b4481ef1fec1980a43d57
Kamakshi <naina.g2070@gmail.com>
2026-07-10 15:00:29 +0530

    Day 8: Added frontend using Django templates

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, "home.html")
+
+
+def recommendations(request):
+    recommendations = [
+        {
+            "title": "Toy Story 2 (1999)",
+            "genres": "Adventure | Animation | Children | Comedy | Fantasy",
+            "rating": 4.3,
+        },
+        {
+            "title": "Monsters, Inc. (2001)",
+            "genres": "Adventure | Animation | Children | Comedy",
+            "rating": 4.2,
+        },
+        {
+            "title": "Shrek (2001)",
+            "genres": "Adventure | Animation | Comedy | Fantasy",
+            "rating": 4.1,
+        },
+    ]
+
+    return render(
+        request,
+        "recommendations.html",
+        {"recommendations": recommendations},
+    )
